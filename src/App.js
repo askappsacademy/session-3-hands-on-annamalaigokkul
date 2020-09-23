073a676fb8fe39afe3515e7d98880f9b718e05f5
annamalaigokkul <70380695+annamalaigokkul@users.noreply.github.com>
2020-09-23 14:41:37 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+// Comment out the below line and click Run to get started with this exercise
+/*
+var reactElementWithoutJSX = React.createElement('div', { id: 'main' }, [
+    React.createElement('h1', { id: 'heading' }, 'Hello World')
+  ]);
+*/
+
+// Now that you have seen how to create an Element, try creating your own element and change the value in line 14 to our new variable
+
+ReactDOM.render(
+  reactElementWithoutJSX,
+  document.getElementById('root')
+);
